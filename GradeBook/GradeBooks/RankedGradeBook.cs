3417f4761b8f26796897997e981874776464468c
johnkenwright0112 <52657403+johnkenwright0112@users.noreply.github.com>
2020-01-03 16:39:08 +0000

    first part

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -5,11 +5,11 @@ using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-    public class StandardGradeBook:BaseGradeBook
+    class RankedGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name) : base(name)
+        public RankedGradeBook(string name) : base(name)
         {
-            Type = GradeBookType.Standard;
+            Type = GradeBookType.Ranked;
         }
     }
 }
